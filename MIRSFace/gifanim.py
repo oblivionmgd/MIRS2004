d58f9fa66d98e3cef96409c166fc090b87be4d10
OblivionMGD <ephixa6022@gmail.com>
2020-10-19 15:23:09 +0900

    add: MIRS face animation code

---
+++ MIRSFace/gifanim.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+root = tk.Tk()
+photo = tk.PhotoImage(file="face.gif")
+gif_index = 0
+
+def next_frame():
+    global gif_index
+    try:
+        photo.configure(format="gif -index {}".format(gif_index))
+
+        gif_index += 1
+    except tk.TclError:
+        gif_index = 0
+        return next_frame()
+    else:
+        root.after(100, next_frame)
+
+label = tk.Label(root, image=photo)
+label.pack()
+
+root.after_idle(next_frame)
+root.mainloop()
