75207925c28e3aa8c4f6676dac343a2345df038c
OblivionMGD <ephixa6022@gmail.com>
2020-11-11 13:31:14 +0900

    Add: Application directory

---
+++ Application/MIRSCTRL/MIRSCTRL/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MIRSCTRL
+//
+//  Created by 中村伸哉 on 2020/11/06.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
