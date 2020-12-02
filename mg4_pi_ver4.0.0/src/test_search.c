b06471e6bfcb606697b17834280fd262d80ab966
sato-ky <atiekiore3103@gmail.com>
2020-12-02 13:52:23 +0900

    update: any file
    update: position.h
    add: stop.c
    update: test_search.c

--- mg4_pi_ver4.0.0/src/test_search.c
+++ mg4_pi_ver4.0.0/src/test_search.c
@@ -7,7 +7,7 @@
 
 int main(){
 	int speed, dist;
-	double dist_l, dist_r, d_l, d_r, d_l2, d_r2, xl, yl, x, y, dir;
+	double dist_l, dist_r, d_l, d_r, d_lo, d_ro, xl, yl, x, y, dir;
 	run_state_t state;
 
   if(arduino_open() != 0) return -1;
@@ -31,9 +31,9 @@ int main(){
   usleep(1000*1000);
 
 	request_get_dist(&dist_l, &dist_r);
-	d_l = dist_l - d_l;
-	d_r = dist_r - d_r;
-	position_set_coord(d_l, d_r);
+	d_lo = dist_l - d_l;
+	d_ro = dist_r - d_r;
+	position_set_coord(d_lo, d_ro);
 
   speed = 30;
 	dist = 400;
@@ -46,18 +46,77 @@ int main(){
   while(1){
     request_get_runmode(&state, &speed, &dist);
 		request_get_dist(&dist_l, &dist_r);
-		d_l2 = dist_l - d_l;
-		d_r2 = dist_r - d_r;
-		position_set_coord(d_l2, d_r2);
+		d_lo = dist_l - d_l;
+		d_ro = dist_r - d_r;
+		d_l = dist_l;
+		d_r = dist_r;
+		position_set_coord(d_lo, d_ro);
 		position_get_coord(&x, &y, &dir);
-		if( x > xl || x < 0){
-			//反転して探索に戻る
 
+		if( x > xl ){
+			dist = 0 - dir;
+		  request_set_runmode(ROT, 45, dist);
+			while(1){
+				request_get_runmode(&state, &speed, &dist);
+		    if( state == STP ) break;
+				usleep(10*1000);
+			}
+			usleep(1000*1000);
+
+			request_get_dist(&dist_l, &dist_r);
+			d_lo = dist_l - d_l;
+			d_ro = dist_r - d_r;
+			position_set_coord(d_lo, d_ro);
 		}
-		if( y > yl || y < 0){
 
+		if( x < 0 ){
+			dist = 0 - ( dir - 180 );
+		  request_set_runmode(ROT, 45, dist);
+			while(1){
+				request_get_runmode(&state, &speed, &dist);
+		    if( state == STP ) break;
+				usleep(10*1000);
+			}
+			usleep(1000*1000);
 
+			request_get_dist(&dist_l, &dist_r);
+			d_lo = dist_l - d_l;
+			d_ro = dist_r - d_r;
+			position_set_coord(d_lo, d_ro);
 		}
+
+		if( y > yl ){
+			dist = 0 - ( dir + 90 ) % 360;
+		  request_set_runmode(ROT, 45, dist);
+			while(1){
+				request_get_runmode(&state, &speed, &dist);
+		    if( state == STP ) break;
+				usleep(10*1000);
+			}
+			usleep(1000*1000);
+
+			request_get_dist(&dist_l, &dist_r);
+			d_lo = dist_l - d_l;
+			d_ro = dist_r - d_r;
+			position_set_coord(d_lo, d_ro);
+		}
+
+		if( y < 0 ){
+			dist = 180 - ( dir + 90 );
+		  request_set_runmode(ROT, 45, dist);
+			while(1){
+				request_get_runmode(&state, &speed, &dist);
+		    if( state == STP ) break;
+				usleep(10*1000);
+			}
+			usleep(1000*1000);
+
+			request_get_dist(&dist_l, &dist_r);
+			d_lo = dist_l - d_l;
+			d_ro = dist_r - d_r;
+			position_set_coord(d_lo, d_ro);
+		}
+
     if( state == STP ) break;
     if(){
       request_set_runmode(STP, 0, 0);
