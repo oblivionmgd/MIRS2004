a8a9afb642fd686e4470176cb2e89c1372df5459
sato-ky <atiekiore3103@gmail.com>
2020-12-11 15:57:23 +0900

    update: some file
    add: C_main.c
    update: pilot.c
    update: stop.c
    update: test_avoid.c
    update: test_fileld.c

--- mg4_pi_ver4.0.0/src/stop.c
+++ mg4_pi_ver4.0.0/src/stop.c
@@ -1,35 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <wiringPi.h>
 #include "request.h"
 #include "stop.h"
-#include "C_send.h"
-// ピン配置
-static const int pin_sw_stop  = 7;
 
 int stop_open(){
-	// GPIOデバイスのオープン
-	if(wiringPiSetupGpio() != 0){
-		printf("failed to open GPIO\n");
-		return -1;
-	}else{
-		// ピン設定
-		pinMode(pin_sw_stop, INPUT);
-		pullUpDnControl(pin_sw_stop, PUD_DOWN);
-		printf("GPIO opened\n");
-		
-		wiringPiISR(pin_sw_stop,INT_EDGE_FALLING,stop);
-		return 0;
-	}
+	request_get_batt(volt);
+		if( volt < 1 ){
+			request_set_runmode(STP, 0, 0);
+			_exit(0);
+			break;
+		}
 }
 
-void stop(){
+/*void stop(){
 	int mode = 11;
 	//int sw_stop;
 	//sw_stop = digitalRead(pin_sw_stop);
 	//printf("switch_stop = %d\n", sw_stop);
 	//usleep(100*1000);
 	request_set_runmode(STP, 0, 0);
-	C_send(mode);
 	_exit(0);
-}
+}*/
