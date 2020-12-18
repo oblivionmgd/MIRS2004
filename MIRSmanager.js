162a0f1a9c4c482100f847050205b79cedea43dc
OblivionMGD <ephixa6022@gmail.com>
2020-12-18 10:50:47 +0900

    upgrade: mirs run command

--- MIRSmanager.js
+++ MIRSmanager.js
@@ -11,13 +11,20 @@ admin.initializeApp({
 var mirsRunCmd = "./Helloworld"
 var db = admin.database();
 var MIRS = db.ref("MIRS");
-var order = db.ref("MIRS/orderStatus");
+var order = db.ref("MIRS/machineStatus");
 // order.set({"status":"run"});
 
 order.on("child_changed",function(snapshot){
-  console.log(exec.execSync(mirsRunCmd).toString());
-  console.log("value Changed!");
-  console.log(snapshot.val());
+  if (snapshot.val() == "go"){
+    //console.log(exec.execSync(mirsRunCmd).toString());
+    console.log("got command:");
+    console.log(snapshot.val());
+  }
+  else {
+    //console.log(exec.execSync(mirsStopCmd).toString());
+    console.log("got command:");
+    console.log(snapshot.val());
+  }
 },
 function(errorObject){
   console.log("failed: " + errorObject.code);
