c9e5243890ef4fd4fb62ff9ee35db46808148b0b
OblivionMGD <ephixa6022@gmail.com>
2021-01-08 14:34:48 +0900

    hoge

--- mg4_pi_ver4.0.0/src/test_request.c
+++ mg4_pi_ver4.0.0/src/test_request.c
@@ -3,78 +3,78 @@
 #include "request.h"
 
 int main(){
-	int mode, speed, dist;
-	double volt;
-	//char buf[256];
-	run_state_t state;
-	conv_state_t conv_state;
+  int mode, speed, dist;
+  double volt;
+  //char buf[256];
+  run_state_t state;
+  conv_state_t conv_state;
 
-	if(arduino_open() != 0) return -1;
+  if(arduino_open() != 0) return -1;
 
 
-	while(1){
-		printf("0:stop  1:straight  2:rotate  3:get_mode  4:get_volt  5:quit\n");
-		scanf("%d",&mode);
+  while(1){
+    printf("0:stop  1:straight  2:rotate  3:get_mode  4:get_volt  5:quit\n");
+    scanf("%d",&mode);
 
-		switch(mode){
-		case 0:
-			request_set_runmode(STP, 0, 0);
-			break;
-		case 1:
-			printf("speed? [cm/s]\n");
-			scanf("%d",&speed);
-			printf("dist? [cm]\n");
-			scanf("%d",&dist);
+    switch(mode){
+      case 0:
+        request_set_runmode(STP, 0, 0);
+        break;
+      case 1:
+        printf("speed? [cm/s]\n");
+        scanf("%d",&speed);
+        printf("dist? [cm]\n");
+        scanf("%d",&dist);
 
-			request_set_runmode(STR, speed, dist);
-			while(1){
-				request_get_runmode(&state, &speed, &dist);
-				if( state == STP ) break;
-			}
+        request_set_runmode(STR, speed, dist);
+        while(1){
+          request_get_runmode(&state, &speed, &dist);
+          if( state == STP ) break;
+        }
 
-			break;
-		case 2:
-			printf("speed? [deg/s]\n");
-			scanf("%d",&speed);
+        break;
+      case 2:
+        printf("speed? [deg/s]\n");
+        scanf("%d",&speed);
 
-			printf("angle? [deg]\n");
-			scanf("%d",&dist);
+        printf("angle? [deg]\n");
+        scanf("%d",&dist);
 
-			request_set_runmode(ROT, speed, dist);
-			while(1){
-				request_get_runmode(&state, &speed, &dist);
-				if( state == STP ) break;
-			}
-			break;
-		case 3:
-			request_get_runmode(&state, &speed, &dist);
-			printf("state = %s\n",((state == STR) ? "STR" : (state == ROT) ? "ROT" : "STP"));
-			break;
-		case 4:
-			request_get_batt(&volt);
-			printf("batt = %4.2lf\n", volt);
-			break;
-		case 5:
-			return 0;
-		default:
-			break;
-		case 6:
-		//request_set_runmode(STR, 30, 100);
-		request_set_convmode(ROTE);
-		printf("set\n");
-		while(1){
-			//request_get_runmode(&state, &speed, &dist);
-			request_get_convmode(&conv_state);
-			printf("get\n");
-			if( conv_state == STOP ){
-				//request_set_runmode(STP, 0, 0);
-				request_set_convmode(STOP);
-				printf("stop\n");
-				break;
-			}
-		}
-	}
-}
-	arduino_close();
-	return 0;
+        request_set_runmode(ROT, speed, dist);
+        while(1){
+          request_get_runmode(&state, &speed, &dist);
+          if( state == STP ) break;
+        }
+        break;
+      case 3:
+        request_get_runmode(&state, &speed, &dist);
+        printf("state = %s\n",((state == STR) ? "STR" : (state == ROT) ? "ROT" : "STP"));
+        break;
+      case 4:
+        request_get_batt(&volt);
+        printf("batt = %4.2lf\n", volt);
+        break;
+      case 5:
+        return 0;
+      default:
+        break;
+      case 6:
+        //request_set_runmode(STR, 30, 100);
+        request_set_convmode(ROTE);
+        printf("set\n");
+        while(1){
+          //request_get_runmode(&state, &speed, &dist);
+          request_get_convmode(&conv_state);
+          printf("get\n");
+          if( conv_state == STOP ){
+            //request_set_runmode(STP, 0, 0);
+            request_set_convmode(STOP);
+            printf("stop\n");
+            break;
+          }
+        }
+    }
+  }
+  arduino_close();
+  return 0;
 }
