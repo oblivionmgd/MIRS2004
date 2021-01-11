00a922257ea1d7862ad94135c710bb24d8a4eb65
sato-ky <atiekiore3103@gmail.com>
2021-01-11 12:57:27 +0900

    Update PV.c

--- mg4_pi_ver4.0.0/src/PV.c
+++ mg4_pi_ver4.0.0/src/PV.c
@@ -6,9 +6,10 @@
 
 int main(){
   int mode, speed, dist;
-  double volt;
+  //double volt;
   //char buf[256];
   run_state_t state;
+  conv_state_t conv_state;
 
   printf("1入れて\n");
   scanf("%d",&mode);
@@ -18,14 +19,16 @@ int main(){
       request_set_convmode(ST);
       while(1){
         request_get_runmode(&state, &speed, &dist);
-        //request_get_convmode();
+        request_get_convmode(&conv_state);
         if( state == STP ) break;
       }
 			usleep(10*1000);
 
       request_set_runmode(STR, 30, 100);
+      request_set_convmode(RT);
       while(1){
         request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
         if( state == STP ) break;
       }
 			usleep(10*1000);
@@ -33,4 +36,6 @@ int main(){
     default:
       break;
   }
+  arduino_close();
+	return 0;
 }
