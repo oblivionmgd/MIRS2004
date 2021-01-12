3b92016aa192ef86b483968d1fa3520e6b2efdf1
sato-ky <atiekiore3103@gmail.com>
2021-01-12 14:49:17 +0900

    Update PV.c

--- mg4_pi_ver4.0.0/src/PV.c
+++ mg4_pi_ver4.0.0/src/PV.c
@@ -15,765 +15,121 @@ int main(){
   scanf("%d",&mode);
   switch(mode){
     case 1:
-      printf("1入れて\n");
-      scanf("%d",&mode);
-      request_set_runmode(ROT, 45, 80);
+      //一個目
+      request_set_runmode(ROT, 45, 90);
       request_set_convmode(ST);
       while(1){
         request_get_runmode(&state, &speed, &dist);
         request_get_convmode(&conv_state);
         if( state == STP ) break;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        usleep(10*1000);
+  			usleep(10*1000);
       }
 			usleep(1000*1000);
-      
+
+      request_set_runmode(ROT, 45, -45);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
       request_set_runmode(STR, 30, 100);
       request_set_convmode(RT);
       while(1){
         request_get_runmode(&state, &speed, &dist);
         request_get_convmode(&conv_state);
         if( state == STP ) break;
-        usleep(10*1000);
+  			usleep(10*1000);
       }
 			usleep(1000*1000);
-      break;
-    case 2:
-      printf("aaa\n");
+
+      //二個目
+      request_set_runmode(ROT, 45, 90);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
+      request_set_runmode(ROT, 45, 90);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
+      request_set_runmode(ROT, 45, -45);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
       request_set_runmode(STR, 30, 100);
       request_set_convmode(RT);
       while(1){
         request_get_runmode(&state, &speed, &dist);
         request_get_convmode(&conv_state);
         if( state == STP ) break;
-        usleep(10*1000);
+  			usleep(10*1000);
       }
-      usleep(1000*1000);
+			usleep(1000*1000);
+
+      //三個目
+      request_set_runmode(ROT, 45, 90);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
+      request_set_runmode(ROT, 45, 90);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
+      request_set_runmode(ROT, 45, 45);
+      request_set_convmode(ST);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
+      request_set_runmode(STR, 30, 100);
+      request_set_convmode(RT);
+      while(1){
+        request_get_runmode(&state, &speed, &dist);
+        request_get_convmode(&conv_state);
+        if( state == STP ) break;
+  			usleep(10*1000);
+      }
+			usleep(1000*1000);
+
+      request_set_convmode(ST);
+      request_get_convmode(&conv_state);
       break;
     default:
       break;
