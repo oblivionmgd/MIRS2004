02ea72779843efce48ccda411e29fb8bff257f7c
OblivionMGD <ephixa6022@gmail.com>
2021-01-21 15:09:23 +0900

    edit: mirsmanager.js

--- MIRSmanager.js
+++ MIRSmanager.js
@@ -8,21 +8,34 @@ admin.initializeApp({
   databaseURL: "https://mirsctrl-js.firebaseio.com"
 });
 
-var mirsRunCmd = "./test_conv"
+var mirsRunCmd
 var mirsStopCmd = "./conv_stop"
 var db = admin.database();
 var MIRS = db.ref("MIRS");
 var order = db.ref("MIRS/machineStatus");
+var ball = db.ref("MIRS/selectedBall");
 // order.set({"status":"run"});
+ball.on("child_changed", function(snapshot){
+  if (snapshot.val() == "tennisball"){
+    mirsRunCmd = "./hoge";
+    console.log("Execution: " + mirsRunCmd.toString());
+  }
+  else {
+    mirsRunCmd = "./fuga";
+    console.log("Execution: " + mirsRunCmd.toString());
+  }
+},
+function(errorObject){
+  console.log("failed:" + errorObject.code);
+});
 
 order.on("child_changed",function(snapshot){
   if (snapshot.val() == "go"){
-    console.log("got command:");
-    console.log(snapshot.val());
     console.log(exec.exec(mirsRunCmd).toString());
   }
   else {
-    exec.exec("killall ./test_conv");
+    console.log("Stopped.")
+    exec.exec("killall " + mirsRunCmd.toString());
     console.log(exec.execSync(mirsStopCmd).toString());
   }
 },
